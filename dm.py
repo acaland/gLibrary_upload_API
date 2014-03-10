23e14dd84d579c2ebb2af7c31e13599e15ac4ce8
Antonio Calanducci <antonio.calanducci@ct.infn.it>
2014-03-10 12:22:51 +0100

    fixing tabs

--- dm.py
+++ dm.py
@@ -11,45 +11,45 @@ dm = Flask(__name__)
 
 
 def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
+				max_age=21600, attach_to_all=True,
+				automatic_options=True):
+	if methods is not None:
+		methods = ', '.join(sorted(x.upper() for x in methods))
+	if headers is not None and not isinstance(headers, basestring):
+		headers = ', '.join(x.upper() for x in headers)
+	if not isinstance(origin, basestring):
+		origin = ', '.join(origin)
+	if isinstance(max_age, timedelta):
+		max_age = max_age.total_seconds()
 
-    def get_methods():
-        if methods is not None:
-            return methods
+	def get_methods():
+		if methods is not None:
+			return methods
 
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
+		options_resp = current_app.make_default_options_response()
+		return options_resp.headers['allow']
 
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
+	def decorator(f):
+		def wrapped_function(*args, **kwargs):
+			if automatic_options and request.method == 'OPTIONS':
+				resp = current_app.make_default_options_response()
+			else:
+				resp = make_response(f(*args, **kwargs))
+			if not attach_to_all and request.method != 'OPTIONS':
+				return resp
 
-            h = resp.headers
+			h = resp.headers
 
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
+			h['Access-Control-Allow-Origin'] = origin
+			h['Access-Control-Allow-Methods'] = get_methods()
+			h['Access-Control-Max-Age'] = str(max_age)
+			if headers is not None:
+				h['Access-Control-Allow-Headers'] = headers
+			return resp
 
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
+		f.provide_automatic_options = False
+		return update_wrapper(wrapped_function, f)
+	return decorator
 
 
 @dm.route("/dav/<vo>/<se>/<path:path>", methods=['PROPFIND','MKCOL','MOVE','HEAD','GET','OPTIONS','PUT', 'DELETE'])
@@ -68,7 +68,7 @@ def browser(vo,se,path):
 		print "replaced", type(request.headers.get('Destination').replace("http://glibrary.ct.infn.it/dm/dav/"+str(vo)+"/"+ str(se), "https://" + str(se)))
 		headers['Destination'] = request.headers.get('Destination').replace("http://glibrary.ct.infn.it/dm/dav/"+str(vo)+"/"+ str(se), "https://" + str(se))  
 		print type(headers['Destination'])
-        headers['Depth'] = request.headers.get('Depth') 
+		headers['Depth'] = request.headers.get('Depth') 
 	for h, v in request.headers:
 		print h, v
 		#headers[h] = v
@@ -87,12 +87,12 @@ def browser(vo,se,path):
 	
 	conn = httplib.HTTPSConnection(se, cert_file=proxy, key_file=proxy)
 	try:
-                conn.request(request.method, '/' + path, None, headers)
-        except Exception, e:
-                #return HttpResponseNotFound("Network error: %s" % e)
-                print "eccezione", e
-                abort(500)
-        resp = conn.getresponse()
+		conn.request(request.method, '/' + path, None, headers)
+	except Exception, e:
+		#return HttpResponseNotFound("Network error: %s" % e)
+		print "eccezione", e
+		abort(500)
+	resp = conn.getresponse()
 	#print "risposta", resp
 	print resp.status, type(resp.status)
 	print resp.reason
@@ -130,7 +130,7 @@ def whoami():
 		print "Environment:", request.environ
 		return "you need to be authenticated"
 	else:
-		return request.environ['SSL_CLIENT_S_DN']	
+		return request.environ['SSL_CLIENT_S_DN']   
 
 @dm.route("/<vo>/<se>/<path:path>")
 def download(vo, se, path):
@@ -148,20 +148,20 @@ def download(vo, se, path):
 	info['voms'] = request.args.get('voms','')
 	#link = "https://%s/%s?authip=%s" % (se, path, request.environ['REMOTE_ADDR'])
 	#print "download link: ", link
-    
-    	print "REMOTE_ADDR: ", request.environ['REMOTE_ADDR']
+	
+	print "REMOTE_ADDR: ", request.environ['REMOTE_ADDR']
 	if 'HTTP_X_FORWARDED_FOR' in request.environ:
 		print "HTTP_X_FORWARDED_FOR: ", request.environ['HTTP_X_FORWARDED_FOR']
 	#print request.META['HTTP_X_FORWARDED_HOST']
 
-        if se in ['infn-se-03.ct.pi2s2.it', 'vega-se.ct.infn.it','se.reef.man.poznan.pl','prod-se-03.ct.infn.it']:
-        	if 'HTTP_X_FORWARDED_FOR' in request.environ:
+	if se in ['infn-se-03.ct.pi2s2.it', 'vega-se.ct.infn.it','se.reef.man.poznan.pl','prod-se-03.ct.infn.it']:
+		if 'HTTP_X_FORWARDED_FOR' in request.environ:
 			headers = {"X-Auth-Ip": request.environ['HTTP_X_FORWARDED_FOR']}
 		else:
 			headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
-		path = "/%s" % path
-        else:
-                headers = {}
+			path = "/%s" % path
+	else:
+		headers = {}
 		if 'HTTP_X_FORWARDED_FOR' in request.environ:
 			path = "/%s?authip=%s" % (path, request.environ['HTTP_X_FORWARDED_FOR'])
 		else:
@@ -195,15 +195,15 @@ def download(vo, se, path):
 	conn.close()
 	return redirect(redirect_url)
 	
-    
+	
 @dm.route("/upload/<vo>/<filename>/<se>/<path:path>")
 def upload(vo, filename, se, path):
 		
 	proxy = get_proxy(vo)
 
-        if se in ['infn-se-03.ct.pi2s2.it', 'vega-se.ct.infn.it','prod-se-03.ct.infn.it']:
-                #headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
-                path = "/%s/%s" % (path, filename)
+	if se in ['infn-se-03.ct.pi2s2.it', 'vega-se.ct.infn.it','prod-se-03.ct.infn.it']:
+		#headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
+		path = "/%s/%s" % (path, filename)
 		conn = httplib.HTTPSConnection(se, cert_file=proxy, key_file=proxy)
 		#conn.set_debuglevel(1)
 		#conn.set_tunnel('localhost', 9090)
@@ -212,26 +212,27 @@ def upload(vo, filename, se, path):
 		datagen, headers = multipart_encode({"myfile": empty_file})
 		data = str().join(datagen) 
 		headers["X-Auth-Ip"] = request.environ['REMOTE_ADDR']
-        	try:
-                	conn.request("POST", path, data, headers)
-        	except Exception, e:
+		try:
+			conn.request("POST", path, data, headers)
+		except Exception, e:
 			print "eccezione", e
-                	#return HttpResponseNotFound("Network error: %s" % e)
-                	abort(500)
+			#return HttpResponseNotFound("Network error: %s" % e)
+			abort(500)
+		
 		resp = conn.getresponse()
-        	print "Status:", resp.status
-        	print resp.reason
-        	print resp.getheaders()
-        	responseTxt = resp.read()
-        	print responseTxt
+		print "Status:", resp.status
+		print resp.reason
+		print resp.getheaders()
+		responseTxt = resp.read()
+		print responseTxt
 		redirect_url = resp.getheader("location")
 		print redirect_url
 		if resp.status == 307:
 			return jsonify({"status": resp.status, "redirect": redirect_url})
 		else:
 			return jsonify({"status": resp.status, "reason": resp.reason, "response": responseTxt}) 
-        else:
-                headers = {}
+	else:
+		headers = {}
 		path = "/%s?metacmd=post&filename=%s&metaopt=755&authip=%s" % (path, filename, request.environ['REMOTE_ADDR'])
 		method = "GET"
 		
@@ -262,7 +263,7 @@ def upload(vo, filename, se, path):
 		resp = {'dest' : dest, 'httpstoken' : httpstoken, 'httpsauthz' : httpsauthz, 'post_url' : url}
 		print resp
 		return jsonify(resp)
-		#return json.dumps(resp)	
+		#return json.dumps(resp)    
 
 
 @dm.route("/put/<vo>/<filename>/<se>/<path:path>", methods=['GET', 'OPTIONS'])
@@ -271,36 +272,38 @@ def put(vo, filename, se, path):
 		
 	proxy = get_proxy(vo)
 
- 	if se in ['infn-se-03.ct.pi2s2.it', 'vega-se.ct.infn.it','se.reef.man.poznan.pl','prod-se-03.ct.infn.it']:
-                if 'HTTP_X_FORWARDED_FOR' in request.environ:
-                        headers = {"X-Auth-Ip": request.environ['HTTP_X_FORWARDED_FOR']}
-                else:
-                        headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
+	if se in ['infn-se-03.ct.pi2s2.it', 'vega-se.ct.infn.it','se.reef.man.poznan.pl','prod-se-03.ct.infn.it']:
+		if 'HTTP_X_FORWARDED_FOR' in request.environ:
+			headers = {"X-Auth-Ip": request.environ['HTTP_X_FORWARDED_FOR']}
+		else:
+			headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
+		
+		print "Delegating IP:", headers['X-Auth-Ip']
+		#headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
+		path = "/%s/%s" % (path, filename)
+		conn = httplib.HTTPSConnection(se, cert_file=proxy, key_file=proxy)
+		#conn.set_debuglevel(1)
+		try:
+			conn.request("PUT", path, None, headers)
+		except Exception, e:
+			print "eccezione", e
+			#return HttpResponseNotFound("Network error: %s" % e)
+			abort(500)
+		
+		resp = conn.getresponse()
+		print "Status:", resp.status
+		print resp.reason
+		print resp.getheaders()
+		responseTxt = resp.read()
+		print responseTxt
+		redirect_url = resp.getheader("location")
+		print redirect_url
+		if resp.status == 307:
+			return jsonify({"status": resp.status, "redirect": redirect_url})
+		else:
+			return jsonify({"status": resp.status, "reason": resp.reason, "response": responseTxt})
 
-                #headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
-                path = "/%s/%s" % (path, filename)
-                conn = httplib.HTTPSConnection(se, cert_file=proxy, key_file=proxy)
-                #conn.set_debuglevel(1)
-                try:
-                        conn.request("PUT", path, None, headers)
-                except Exception, e:
-                        print "eccezione", e
-                        #return HttpResponseNotFound("Network error: %s" % e)
-                        abort(500)
-                resp = conn.getresponse()
-                print "Status:", resp.status
-                print resp.reason
-                print resp.getheaders()
-                responseTxt = resp.read()
-                print responseTxt
-                redirect_url = resp.getheader("location")
-                print redirect_url
-                if resp.status == 307:
-                        return jsonify({"status": resp.status, "redirect": redirect_url})
-                else:
-                        return jsonify({"status": resp.status, "reason": resp.reason, "response": responseTxt})
-
-	else:	
+	else:   
 		path = "/%s?metacmd=put&filename=%s&metaopt=755&authip=%s" % (path, filename, request.environ['REMOTE_ADDR'])
 		print "se: ", se
 		print "path:", path
@@ -328,11 +331,11 @@ def put(vo, filename, se, path):
 		resp = {'dest' : dest, 'httpstoken' : httpstoken, 'httpsauthz' : httpsauthz, 'put_url' : url}
 		print resp
 		return jsonify(resp)
-		#return json.dumps(resp)	
+		#return json.dumps(resp)    
 
 
 @dm.route("/putdone/<se>/<path:path>")
-#@dm.route("/putdone/<vo>/<filename>/<se>/<path:path>")	
+#@dm.route("/putdone/<vo>/<filename>/<se>/<path:path>") 
 #def putdone(vo, filename, se, path):
 def putdone(se, path, vo="", filename=""):
 	
@@ -378,26 +381,26 @@ def putdone(se, path, vo="", filename=""):
 	
 @dm.route('/sign_s3/')
 def sign_s3():
-    AWS_ACCESS_KEY = 'AKIAJTM37DDJONDPAT5Q'       
-    AWS_SECRET_KEY = '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' 
-    S3_BUCKET = 'etnatraining' 
+	AWS_ACCESS_KEY = 'AKIAJTM37DDJONDPAT5Q'       
+	AWS_SECRET_KEY = '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' 
+	S3_BUCKET = 'etnatraining' 
 
-    object_name = request.args.get('s3_object_name')
-    mime_type = request.args.get('s3_object_type')
+	object_name = request.args.get('s3_object_name')
+	mime_type = request.args.get('s3_object_type')
 
-    expires = int(time.time()+60)
-    amz_headers = "x-amz-acl:public-read"
+	expires = int(time.time()+60)
+	amz_headers = "x-amz-acl:public-read"
 
-    put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+	put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
 
-    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha).digest())
+	signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha).digest())
 
-    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+	url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
 
-    return json.dumps({
-        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
-         'url': url
-    })	
+	return json.dumps({
+		'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+		 'url': url
+	})  
 		
 	
 
@@ -405,54 +408,53 @@ def sign_s3():
 def get_proxy(vo):
 
 	if vo == "vo.indicate-project.eu":
-         	proxy_file = '/tmp/indicate_proxy'
-                robot_serial = '26467'
-                certificate_md5 = '678db46f8ccd12ccb240cd2f91d18205'
-                attribute = '/vo.indicate-project.eu'
-        elif vo == "vo.aginfra.eu":
-                robot_serial = '25667'
-                attribute = '/vo.aginfra.eu'
-                certificate_md5 = '62b53afcb320386d6ad938d3d2fdfbfc'
-                proxy_file = '/tmp/aginfra_proxy'
-        elif vo == "vo.earthserver.eu":
-                robot_serial = "25668"
-                proxy_file = '/tmp/earthserver_proxy'
-                certificate_md5 = '36beeec99546392a0fd6692242fef938'
-                attribute = '/vo.earthserver.eu'
-        elif vo == "vo.dch-rp.eu" or vo == 'vo.indicate-project.eu':
-                robot_serial = '26581'
-                proxy_file = '/tmp/dchrp_proxy'
-                certificate_md5 = '876149964d57df2310eb3d398f905749'
-                attribute='/vo.dch-rp.eu'
+		proxy_file = '/tmp/indicate_proxy'
+		robot_serial = '26467'
+		certificate_md5 = '678db46f8ccd12ccb240cd2f91d18205'
+		attribute = '/vo.indicate-project.eu'
+	elif vo == "vo.aginfra.eu":
+		robot_serial = '25667'
+		attribute = '/vo.aginfra.eu'
+		certificate_md5 = '62b53afcb320386d6ad938d3d2fdfbfc'
+		proxy_file = '/tmp/aginfra_proxy'
+	elif vo == "vo.earthserver.eu":
+		robot_serial = "25668"
+		proxy_file = '/tmp/earthserver_proxy'
+		certificate_md5 = '36beeec99546392a0fd6692242fef938'
+		attribute = '/vo.earthserver.eu'
+	elif vo == "vo.dch-rp.eu" or vo == 'vo.indicate-project.eu':
+		robot_serial = '26581'
+		proxy_file = '/tmp/dchrp_proxy'
+		certificate_md5 = '876149964d57df2310eb3d398f905749'
+		attribute='/vo.dch-rp.eu'
 	elif vo == "vo.eu-decide.eu":
 		robot_serial = '28895'
-                proxy_file = '/tmp/decide_proxy'
-                certificate_md5 = '2ce14167e631d8bd1fb4a5f2b86602e0'
-                attribute='/vo.eu-decide.eu'
+		proxy_file = '/tmp/decide_proxy'
+		certificate_md5 = '2ce14167e631d8bd1fb4a5f2b86602e0'
+		attribute='/vo.eu-decide.eu'
 	elif vo == "eumed":
-                robot_serial = '27696'
-                proxy_file = '/tmp/eumed_proxy'
-                certificate_md5 = 'bc681e2bd4c3ace2a4c54907ea0c379b'
-                attribute='/eumed'
-        else:
-                robot_serial = '25207'
-                proxy_file = '/tmp/cataniasg_proxy'
-                certificate_md5 = '332576f78a4fe70a52048043e90cd11f'
-                attribute = '/' + vo
+		robot_serial = '27696'
+		proxy_file = '/tmp/eumed_proxy'
+		certificate_md5 = 'bc681e2bd4c3ace2a4c54907ea0c379b'
+		attribute='/eumed'
+	else:
+		robot_serial = '25207'
+		proxy_file = '/tmp/cataniasg_proxy'
+		certificate_md5 = '332576f78a4fe70a52048043e90cd11f'
+		attribute = '/' + vo
 
-    	#print request.environ
-    	print "call to get_proxy"
-    	#etokenserver = "myproxy.ct.infn.it"
-
-    	#server_url = "http://myproxy.ct.infn.it:8082/eTokenServer/eToken/%s?voms=%s:%s&proxy-renewal=false&disable-voms-proxy=true" % (certificate_serial, vo, attribute)
-    	server_url = "http://etokenserver.ct.infn.it:8082/eTokenServer/eToken/%s?voms=%s:/%s&proxy-renewal=false&disable-voms-proxy=true" % (certificate_md5, vo, attribute)
-    	print server_url
-    	f = urllib.urlopen(server_url)
-    	proxy = open(proxy_file, "w");
-    	proxy.write(f.read())
-    	f.close()
-    	proxy.close()
-    	os.chmod(proxy_file, 0600)
+	#print request.environ
+	print "call to get_proxy"
+	#etokenserver = "myproxy.ct.infn.it"
+	#server_url = "http://myproxy.ct.infn.it:8082/eTokenServer/eToken/%s?voms=%s:%s&proxy-renewal=false&disable-voms-proxy=true" % (certificate_serial, vo, attribute)
+	server_url = "http://etokenserver.ct.infn.it:8082/eTokenServer/eToken/%s?voms=%s:/%s&proxy-renewal=false&disable-voms-proxy=true" % (certificate_md5, vo, attribute)
+	print server_url
+	f = urllib.urlopen(server_url)
+	proxy = open(proxy_file, "w");
+	proxy.write(f.read())
+	f.close()
+	proxy.close()
+	os.chmod(proxy_file, 0600)
 	return proxy_file
 
 
