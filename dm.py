b968f404f2bd6b07d61d1c347ca26d30bf98ee71
Antonio Calanducci <antonio.calanducci@ct.infn.it>
2016-03-04 13:13:36 +0100

    adding logs with logger

--- dm.py
+++ dm.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, abort, jsonify, make_response, current_app, send_from_directory
-import json, re, urllib, httplib, os, time, base64, hmac, sha, requests
+import json, re, urllib, httplib, os, time, base64, hmac, sha, requests, logging
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
 from hashlib import sha1
@@ -56,7 +56,7 @@ def crossdomain(origin=None, methods=None, headers=None,
 @dm.route("/dav/<vo>/<se>/<path:path>", methods=['PROPFIND','MKCOL','MOVE','HEAD','GET','OPTIONS','PUT', 'DELETE'])
 def browser(vo,se,path):
 	print "DAV browser"
-	print "request method>", request.method 
+	print "request method>", request.method
 	print "request headers>"
 	headers = {}
 	if request.method == 'MOVE':
@@ -67,10 +67,10 @@ def browser(vo,se,path):
 		print "pattern", type("http://glibrary.ct.infn.it/dm/dav/"+str(vo)+"/"+ str(se))
 		print "replacement", type("https://" + str(se))
 		print "replaced", type(request.headers.get('Destination').replace("http://glibrary.ct.infn.it/dm/dav/"+str(vo)+"/"+ str(se), "https://" + str(se)))
-		headers['Destination'] = request.headers.get('Destination').replace("http://glibrary.ct.infn.it/dm/dav/"+str(vo)+"/"+ str(se), "https://" + str(se))  
+		headers['Destination'] = request.headers.get('Destination').replace("http://glibrary.ct.infn.it/dm/dav/"+str(vo)+"/"+ str(se), "https://" + str(se))
 		print type(headers['Destination'])
 	if request.headers.get('Depth'):
-		headers['Depth'] = request.headers.get('Depth') 
+		headers['Depth'] = request.headers.get('Depth')
 	for h, v in request.headers:
 		print h, v
 		#headers[h] = v
@@ -86,7 +86,7 @@ def browser(vo,se,path):
 	print "path:", "/" + path
 	#headers = {}
 	#headers['Depth'] = 1
-	
+
 	conn = httplib.HTTPSConnection(se, cert_file=proxy, key_file=proxy)
 	try:
 		conn.request(request.method, '/' + path, None, headers)
@@ -121,14 +121,18 @@ def browser(vo,se,path):
 		response.status_code = resp.status
 		#print "after status", response
 		print "sto restituendo la risposta", resp.status
-		return response 
+		return response
 		#abort(500)
 
 
 @dm.route("/hello")
 def hello():
 	#abort(500)
-	return jsonify({'success': True, 'text': "Hello, World!"}) 
+
+	dm.logger.info('API /hello')
+
+
+	return jsonify({'success': True, 'text': "Hello, World 22!"})
 
 @dm.route("/whoami")
 def whoami():
@@ -136,7 +140,7 @@ def whoami():
 		print "Environment:", request.environ
 		return "you need to be authenticated"
 	else:
-		return request.environ['SSL_CLIENT_S_DN']   
+		return request.environ['SSL_CLIENT_S_DN']
 
 @dm.route("/<vo>/<se>/<path:path>", methods=['GET','PUT'])
 def download(vo, se, path):
@@ -144,19 +148,19 @@ def download(vo, se, path):
 		print "Download API"
 		print "Cookies:", request.cookies
 		print "VO: ", vo
-			
+
 		#get_proxy(robot_serial, vo, attribute, proxy)
 		proxy = get_proxy(vo)
-		
+
 		info = {}
 		info['se'] = se
 		info['path'] = path
 		#print request.args.get('pippo','')
-		info['robot'] = request.args.get('robot_serial','') 
+		info['robot'] = request.args.get('robot_serial','')
 		info['voms'] = request.args.get('voms','')
 		#link = "https://%s/%s?authip=%s" % (se, path, request.environ['REMOTE_ADDR'])
 		#print "download link: ", link
-		
+
 		print "REMOTE_ADDR: ", request.environ['REMOTE_ADDR']
 		if 'HTTP_X_FORWARDED_FOR' in request.environ:
 			print "HTTP_X_FORWARDED_FOR: ", request.environ['HTTP_X_FORWARDED_FOR']
@@ -177,7 +181,7 @@ def download(vo, se, path):
 				path = "/%s?authip=%s" % (path, request.environ['REMOTE_ADDR'])
 
 
-		
+
 		#path = "/%s?authip=%s" % (path, request.environ['REMOTE_ADDR'])
 		print "SE:", se
 		print "PATH:", path
@@ -211,11 +215,11 @@ def download(vo, se, path):
 		print "filename", filename
 		newpath = '/'.join(path_parts[:-1])
 		return put(vo, filename, se, newpath)
-	
-	
+
+
 @dm.route("/upload/<vo>/<filename>/<se>/<path:path>")
 def upload(vo, filename, se, path):
-		
+
 	proxy = get_proxy(vo)
 
 	if se in ['infn-se-03.ct.pi2s2.it', 'se01.grid.arn.dz','prod-se-03.ct.infn.it']:
@@ -227,7 +231,7 @@ def upload(vo, filename, se, path):
 		empty_file = open("/tmp/empty", 'wb+')
 		#register_openers()
 		datagen, headers = multipart_encode({"myfile": empty_file})
-		data = str().join(datagen) 
+		data = str().join(datagen)
 		headers["X-Auth-Ip"] = request.environ['REMOTE_ADDR']
 		try:
 			conn.request("POST", path, data, headers)
@@ -235,7 +239,7 @@ def upload(vo, filename, se, path):
 			print "eccezione", e
 			#return HttpResponseNotFound("Network error: %s" % e)
 			abort(500)
-		
+
 		resp = conn.getresponse()
 		print "Status:", resp.status
 		print resp.reason
@@ -247,12 +251,12 @@ def upload(vo, filename, se, path):
 		if resp.status == 307:
 			return jsonify({"status": resp.status, "redirect": redirect_url})
 		else:
-			return jsonify({"status": resp.status, "reason": resp.reason, "response": responseTxt}) 
+			return jsonify({"status": resp.status, "reason": resp.reason, "response": responseTxt})
 	else:
 		headers = {}
 		path = "/%s?metacmd=post&filename=%s&metaopt=755&authip=%s" % (path, filename, request.environ['REMOTE_ADDR'])
 		method = "GET"
-		
+
 		#path = "/%s?metacmd=post&filename=%s&metaopt=755&authip=%s" % (path, filename, request.environ['REMOTE_ADDR'])
 		print "se: ", se
 		print "path:", path
@@ -280,22 +284,24 @@ def upload(vo, filename, se, path):
 		resp = {'dest' : dest, 'httpstoken' : httpstoken, 'httpsauthz' : httpsauthz, 'post_url' : url}
 		print resp
 		return jsonify(resp)
-		#return json.dumps(resp)    
+		#return json.dumps(resp)
 
 
 @dm.route("/put/<vo>/<filename>/<se>/<path:path>", methods=['GET', 'OPTIONS'])
 @crossdomain(origin='*', headers=['Content-Type','Content-Disposition','X-Requested-With','X-File-Type','X-File-Name','X-File-Size'])
 def put(vo, filename, se, path):
-		
-	proxy = get_proxy(vo)
 
+	proxy = get_proxy(vo)
+	dm.logger.info("\nUPLOAD API: Request for a PUT URL for '%s' in '%s' at '%s'", filename, se, path)
 	if se in ['infn-se-03.ct.pi2s2.it', 'se01.grid.arn.dz','se.reef.man.poznan.pl','prod-se-03.ct.infn.it','gridsrv3-4.dir.garr.it']:
 		if 'HTTP_X_FORWARDED_FOR' in request.environ:
 			headers = {"X-Auth-Ip": request.environ['HTTP_X_FORWARDED_FOR']}
 		else:
 			headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
-		
+
 		print "Delegating IP:", headers['X-Auth-Ip']
+		dm.logger.info("Requesting client %s", headers['X-Auth-Ip'])
+
 		#headers = {"X-Auth-Ip": request.environ['REMOTE_ADDR']}
 		path = "/%s/%s" % (path, filename)
 		print "path: %s " % path
@@ -306,25 +312,34 @@ def put(vo, filename, se, path):
 		except Exception, e:
 			print "eccezione", e
 			#return HttpResponseNotFound("Network error: %s" % e)
+			dm.logger.error("Request for a PUT url to the storage '%s' failed", se)
 			abort(500)
-		
+
 		resp = conn.getresponse()
 		print "Status:", resp.status
 		print resp.reason
-		print resp.getheaders()
+
+		dm.logger.debug("response from '%s': Status '%s' Reason '%s'", se, resp.status, resp.reason)
+		#dm.logger.debug("response headers %s", resp.getheaders())
 		responseTxt = resp.read()
-		print responseTxt
+		#print responseTxt
+		#dm.logger.debug("response body %s", responseTxt)
 		redirect_url = resp.getheader("location")
 		print redirect_url
+		dm.logger.info("PUT URL for upload: '%s'", redirect_url)
+
 		if resp.status == 307:
 			return jsonify({"status": resp.status, "redirect": redirect_url})
 		else:
+			dm.logger.error(responseTxt)
 			return jsonify({"status": resp.status, "reason": resp.reason, "response": responseTxt}), resp.status
 
-	else:   
+	else:
+		dm.logger.info("using old DPM SE redirect with authip. Update your storage SE")
 		path = "/%s?metacmd=put&filename=%s&metaopt=755&authip=%s" % (path, filename, request.environ['REMOTE_ADDR'])
 		print "se: ", se
 		print "path:", path
+		dm.logger.info("SE %s, Path %s", se, path)
 		conn = httplib.HTTPSConnection(se, cert_file=proxy, key_file=proxy)
 		try:
 			conn.request("GET", path)
@@ -334,12 +349,16 @@ def put(vo, filename, se, path):
 		resp = conn.getresponse()
 		print resp.status
 		print resp.reason
-		print resp.getheaders()
+		#print resp.getheaders()
 		responseTxt = resp.read()
 		print responseTxt
+		dm.logger.debug("response from SE %s: Status %s, Reason %s", se, resp.status, rest.reason)
+		dm.logger.debug("response headers", resp.getheaders())
+		dm.logger.debug("response body", responseTxt)
 		regex = re.compile('.+action=\"(.+)\".+')
 		action_url=regex.search(responseTxt)
 		if action_url == None:
+			dm.logger.error("failed to return a redirect URL for PUT", responseTxt)
 			return jsonify({'success': False, 'error': responseTxt})
 		url = action_url.group(1)
 		parsed_url = re.search('(http://.*)\?httpstoken=(.*)&httpsauthz=(.*)', url)
@@ -348,32 +367,33 @@ def put(vo, filename, se, path):
 		httpsauthz = parsed_url.group(3)
 		resp = {'dest' : dest, 'httpstoken' : httpstoken, 'httpsauthz' : httpsauthz, 'put_url' : url}
 		print resp
+		dm.logger.info("redirect URL for PUT upload", url)
 		return jsonify(resp)
-		#return json.dumps(resp)    
+		#return json.dumps(resp)
 
 
 @dm.route("/putdone/<se>/<path:path>")
-#@dm.route("/putdone/<vo>/<filename>/<se>/<path:path>") 
+#@dm.route("/putdone/<vo>/<filename>/<se>/<path:path>")
 #def putdone(vo, filename, se, path):
 def putdone(se, path, vo="", filename=""):
-	
-	print "sono qui" 
+
+	print "sono qui"
 	print "vo: ", vo
 	print "se: ", se
 	print "path: ", path
 	print "filename: ", filename
-	
+
 	dpmtoken = request.args.get('dpmtoken','');
 	print "sono in putdone", dpmtoken
 	if not dpmtoken:
 		return jsonify({'success': False, 'error': "No dpmtoken provided"});
-	
+
 	if vo == "":
 		vo = "vo.indicate-project.eu"
-		
+
 	proxy = get_proxy(vo)
 	print "dopo proxy"
-	
+
 	if filename:
 		path = "/%s/%s?metacmd=putdone&dpmtoken=%s" % (path, filename, dpmtoken)
 	else:
@@ -387,7 +407,7 @@ def putdone(se, path, vo="", filename=""):
 	except Exception, e:
 		#return HttpResponseNotFound("Network error: %s" % e)
 		abort(404)
-	
+
 	resp = conn.getresponse()
 	#print resp.status
 	#print resp.reason
@@ -395,7 +415,7 @@ def putdone(se, path, vo="", filename=""):
 	responseTxt = resp.read()
 	#print responseTxt
 	return jsonify({'success': True, 'response': resp.reason});
-	
+
 @dm.route('/warc/extract/<vo>/<se>/<path:path>/<offset>')
 def warc(vo, se, path, offset):
 
@@ -413,7 +433,7 @@ def warc(vo, se, path, offset):
 	print "url:", url
 	r = requests.get(url, stream=True, cert=proxy, verify=False)
 	with open(local_filename, 'wb') as f:
-		for chunk in r.iter_content(chunk_size=1024): 
+		for chunk in r.iter_content(chunk_size=1024):
 			if chunk: # filter out keep-alive new chunks
 				f.write(chunk)
 				f.flush()
@@ -430,12 +450,12 @@ def warc(vo, se, path, offset):
 
 	return send_from_directory('/tmp','warc_dump')
 
-	
+
 @dm.route('/sign_s3/')
 def sign_s3():
-	AWS_ACCESS_KEY = 'AKIAJTM37DDJONDPAT5Q'       
-	AWS_SECRET_KEY = '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' 
-	S3_BUCKET = 'etnatraining' 
+	AWS_ACCESS_KEY = 'AKIAJTM37DDJONDPAT5Q'
+	AWS_SECRET_KEY = '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'
+	S3_BUCKET = 'etnatraining'
 
 	object_name = request.args.get('s3_object_name')
 	mime_type = request.args.get('s3_object_type')
@@ -452,11 +472,11 @@ def sign_s3():
 	return json.dumps({
 		'signed_request': '%s?AWSAccessKeyId=%s&Expires=%d&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
 		 'url': url
-	})  
-	
-@dm.route('/cloud/<host>/<path:path>', methods=['GET','PUT'])	
+	})
+
+@dm.route('/cloud/<host>/<path:path>', methods=['GET','PUT'])
 def swift(host, path):
-	seconds = 10
+	seconds = 120
 	expires = int(time.time() + int(seconds))
 	if not path.startswith('/'):
 		path = '/' + path
@@ -474,8 +494,8 @@ def swift(host, path):
 	print "path corretto"
 	account = parts[2]
 	keys = {
-		"AUTH_51b2f4e508144fa5b0c28f02b1618bfd":"",
-		"glibrary":""
+		"AUTH_51b2f4e508144fa5b0c28f02b1618bfd":"correcthorsebatterystaple",
+		"AUTH_3729798f1d494dcba22abe9763c22258":"4paxXm3tar94T62oGrlQSi5bE4o5mcA1"
 	}
 	try:
 		print "(method, key, expires, path)=", request.method, keys[account], expires, path
@@ -485,7 +505,7 @@ def swift(host, path):
 	except Exception, e:
 		response = jsonify({"error": "no valid key found for the account %s" % account})
 		response.status_code = 400
-		return response	
+		return response
 
 
 def get_proxy(vo):
@@ -540,7 +560,8 @@ def get_proxy(vo):
 		attribute = '/' + vo
 
 	#print request.environ
-	print "call to get_proxy"
+	##print "call to get_proxy"
+	dm.logger.info("Retrieving a proxy from etokenserver.ct.infn.it for VO '%s'", vo)
 	#etokenserver = "myproxy.ct.infn.it"
 	#server_url = "http://myproxy.ct.infn.it:8082/eTokenServer/eToken/%s?voms=%s:%s&proxy-renewal=false&disable-voms-proxy=true" % (certificate_serial, vo, attribute)
 	server_url = "http://etokenserver.ct.infn.it:8082/eTokenServer/eToken/%s?voms=%s:%s&proxy-renewal=false&disable-voms-proxy=%s&rfc-proxy=true&cn-label=eToken:Empty" % (certificate_md5, vo, attribute, disable_voms)
@@ -551,6 +572,7 @@ def get_proxy(vo):
 	f.close()
 	proxy.close()
 	os.chmod(proxy_file, 0600)
+	dm.logger.info("Proxy stored in '%s'", proxy_file)
 	return proxy_file
 
 
@@ -558,6 +580,14 @@ def get_proxy(vo):
 
 
 if __name__ == "__main__":
+	# Configure logs
+	print "set log handler"
+	handler = logging.FileHandler('/tmp/glibrary.log')
+	handler.setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	handler.setFormatter(formatter)
+	dm.logger.addHandler(handler)
+	print "log handler set"
 	dm.run(host='0.0.0.0', port=8000, debug=True)
 
 #opener = robot_init()
--- dm.wsgi
+++ dm.wsgi
@@ -1,4 +1,4 @@
-import sys
+import sys, logging
 
 path = '/var/www/html/api/dm'
 
@@ -6,3 +6,10 @@ if path not in sys.path:
     sys.path.insert(0, path)
 
 from dm import dm as application
+
+print "setting logger handler"
+handler = logging.FileHandler('/var/www/html/api/glibrary.log')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+application.logger.setLevel(logging.DEBUG)
+application.logger.addHandler(handler)
